453c71a58d88fcc1d77b72ffe1fd0db5f2e65dec
Dimas Gabriel <dimas@dimasgabriel.net>
2024-06-19 14:32:15 -0300

    Initial Commit

---
+++ Pocket/PocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketApp.swift
+//  Pocket
+//
+//  Created by Dimas Gabriel on 6/19/24.
+//
+
+import SwiftUI
+
+@main
+struct PocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PocketTests/PocketTests.swift
@@ -0,0 +1,16 @@
+//
+//  PocketTests.swift
+//  PocketTests
+//
+//  Created by Dimas Gabriel on 6/19/24.
+//
+
+import Testing
+
+struct PocketTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
