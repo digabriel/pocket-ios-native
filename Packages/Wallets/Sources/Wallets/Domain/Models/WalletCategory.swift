a8cb30126f8d4ad730884a15412b11844b48d2b5
Dimas Gabriel <dimas@dimasgabriel.net>
2024-07-04 09:52:57 -0300

    Add WalletCategory model

---
+++ Packages/Wallets/Sources/Wallets/Domain/Models/WalletCategory.swift
@@ -0,0 +1,22 @@
+//
+//  WalletCategory.swift
+//  
+//
+//  Created by Dimas Gabriel on 7/4/24.
+//
+
+import SwiftData
+
+@Model public struct WalletCategory {
+    enum Value: String, Codable, CaseIterable {
+        case spending
+        case savings
+        case debt
+    }
+
+    let value: Value
+
+    init(value: Value) {
+        self.value = value
+    }
+}
