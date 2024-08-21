08c124c39cb3295c4fa0eeb4777e1c7bf78053e4
Dimas Gabriel <dimas@dimasgabriel.net>
2024-08-21 11:50:54 -0300

    Create a new Toggle component in the Styleguide

---
+++ Packages/Styleguide/Sources/Styleguide/Components/Toggle.swift
@@ -0,0 +1,53 @@
+//
+//  Toggle.swift
+//  Styleguide
+//
+//  Created by Dimas Gabriel on 8/21/24.
+//
+
+import SwiftUI
+
+struct Toggle: View {
+    let title: String
+    let description: String?
+
+    @Binding private var isEnabled: Bool
+
+    init(title: String, description: String? = nil, isEnabled: Binding<Bool>) {
+        self.title = title
+        self.description = description
+        self._isEnabled = isEnabled
+    }
+
+    var body: some View {
+        HStack(spacing: Dimensions.shared.five) {
+            VStack(alignment: .leading, spacing: Dimensions.shared.three) {
+                Text(title)
+                    .foregroundStyle(Color.regular.black)
+                    .font(Font.title.smallRounded)
+
+                if let description {
+                    Text(description)
+                        .foregroundStyle(Color.regular.gray)
+                        .font(Font.text.small)
+                }
+            }
+
+            Spacer()
+
+            SwiftUI.Toggle("", isOn: $isEnabled)
+                .labelsHidden()
+        }
+    }
+}
+
+#Preview {
+    @Previewable @State var isEnabled = false
+
+    Toggle(title: "A Toggle", description: "A toggle description", isEnabled: $isEnabled)
+        .padding()
+
+    Toggle(title: "A Toggle without description", isEnabled: $isEnabled)
+        .padding()
+        .tint(Color.regular.yellow)
+}
