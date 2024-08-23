88276661f322441d8983ac805fc7c62001bed015
Dimas Gabriel <dimas@dimasgabriel.net>
2024-08-23 14:17:02 -0300

    Expose color resource names for Styleguide colors

--- Packages/Styleguide/Sources/Styleguide/Colors.swift
+++ Packages/Styleguide/Sources/Styleguide/Colors.swift
@@ -44,4 +44,35 @@ public extension Color {
     struct Wallet: Sendable {
         public let accent = Color(ColorResource.walletsAccent)
     }
+
+    var name: String? {
+        switch self {
+        case Color.background.pastel: return "backgroundPastel"
+        case Color.background.lightGray: return "backgroundLightGray"
+        case Color.background.darkPink: return "backgroundDarkPink"
+        case Color.background.lightPink: return "backgroundLightPink"
+
+        case Color.regular.orange: return "orange"
+        case Color.regular.black: return "black"
+        case Color.regular.white: return "white"
+        case Color.regular.gray: return "gray"
+        case Color.regular.yellow: return "yellow"
+        case Color.regular.dark: return "dark"
+        case Color.regular.lightBlue: return "lightBlue"
+        case Color.regular.pastelRed: return "pastelRed"
+        case Color.regular.purple: return "purple"
+        case Color.regular.lightGray: return "lightGray"
+        case Color.regular.blueGreen: return "blueGreen"
+        case Color.regular.pink: return "pink"
+        case Color.regular.green: return "green"
+        case Color.regular.darkGreen: return "darkGreen"
+        case Color.regular.blue: return "blue"
+        case Color.regular.red: return "red"
+        case Color.regular.darkGray: return "darkGray"
+        case Color.regular.softGray: return "softGray"
+
+        case Color.wallet.accent: return "walletsAccent"
+        default: return nil
+        }
+    }
 }
